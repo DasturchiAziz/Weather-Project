3d2f43713bb26cbe1b3acbc4fd28e2636c5a1adc
yoshDasturchi <AzGitHub5770@gmail.com>
2022-11-01 11:51:56 +0500

    Weather news

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function loder (sestem){
 
 const UpdateUi = (weather)=>{
    details.innerHTML = `
-   <h5 class="mb-3">${weather.name},${weather.sys.country} 🌏</h5>
+   <h5 class="mb-3 text-city">${weather.name},${weather.sys.country} 🌏</h5>
    <p class="mb-3 clear">${weather.weather[0].main} ⛅</p>
    <div class="display-4 mb-3">
      <span class="temp">${Math.round(weather.main.temp)}</span>
